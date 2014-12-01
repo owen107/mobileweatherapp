2a2c92ef401fecb578ab04753bd40869165e5fb1
owen107 <owenxu107@gmail.com>
2014-12-01 03:02:55 -0500

    Finished extended weather info page

--- builds/development/js/app.js
+++ builds/development/js/app.js
@@ -39,12 +39,16 @@ var icons = { "clear-day" : "B",
                 currentWeather(result);
                 currentSpecific(result);
                 getDailyWeather(result);
+                dailyWeatherDetails(result);
+                
                 getHourlyWeather(result);
-                var time = getCurrentTime(result);
                 
               },
               error: function(result) {
                 console.log(e.message);
+              },
+              complete: function(event) {
+                $('#hide').hide();
               }
            });
        }
@@ -119,18 +123,9 @@ var icons = { "clear-day" : "B",
              var output = '<li>';
 
              var temp = Math.round(result[index].temperature) + "&#176;";
-             var icon_key = result[index].icon;
+             var icon_key = result[index].icon;     
              var t = new Date(result[index].time * 1000);
-             var hour = t.getHours();
-             if (hour === 0) {
-                hour = 12 + 'AM';
-             }  else if ( hour > 0 && hour < 12) {
-                hour += 'AM';
-             }  else if ( hour === 12) {
-                hour += 'PM'; 
-             }  else {
-                hour = (hour - 12) + 'PM';
-             }
+             var hour = convertHour(t);
              if (index == 0) {
                output += '<span class="hour">Now</span>';
              } else {
@@ -140,6 +135,21 @@ var icons = { "clear-day" : "B",
              output += '<span class="hour-temp">' + temp + '</span></li>';
              $('#hourly ul').append(output);
          });
+      }
+
+      function convertHour(time) {
+
+         var hour = time.getHours();
+         if (hour === 0) {
+            hour = 12 + 'AM';
+         }  else if ( hour > 0 && hour < 12) {
+            hour += 'AM';
+         }  else if ( hour === 12) {
+            hour += 'PM'; 
+         }  else {
+            hour = (hour - 12) + 'PM';
+         }
+         return hour;
       }
 
       function getDailyWeather(data) {
@@ -180,8 +190,104 @@ var icons = { "clear-day" : "B",
          $('#daily ul').append($hide);
       }
 
+      function convertHourMin(time) {
+
+         var hour = time.getHours();
+         var min = time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes();
+         var hourMin = '';
+
+         if (hour === 0) {
+            hourMin = 12 + ':' + min + 'AM';
+         }  else if ( hour > 0 && hour < 12) {
+            hourMin = hour + ':' + min + 'AM';
+         }  else if ( hour === 12) {
+            hourMin = hour + ':' + min + 'PM'; 
+         }  else {
+            hourMin = (hour - 12) + ':' + min +  'PM';
+         }
+         return hourMin;
+      }
+
       function dailyWeatherDetails(data) {
-        
+          var result = data.daily.data;
+          var element = $('#days_extended');
+          element.html('');
+
+          $.each(result, function(index) {
+             var output = '<div class="details"><div class="group"><div class="day_info">';
+
+             var srt = new Date(result[index].sunriseTime * 1000);
+             var sst = new Date(result[index].sunsetTime * 1000);
+             var minTempTime = new Date(result[index].temperatureMinTime * 1000);
+             var maxTempTime = new Date(result[index].temperatureMaxTime * 1000);
+             var t = new Date(result[index].time * 1000);
+
+             var day;
+             var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+             var months = ['January','Feburary','March','April','May','June','July','August','September','October','November','December'];
+             if (index == 0) {
+                day = 'Today';
+             } else {
+                day = days[t.getDay()];
+             }
+             var month = months[t.getMonth()];
+             var date = t.getDate();
+             var md = month + ' ' + date;
+             
+             var srhour = convertHourMin(srt);
+             var sshour = convertHourMin(sst);
+             var minTempHour = convertHour(minTempTime);
+             var maxTempHour = convertHour(maxTempTime);
+
+             var minTemp = Math.round(result[index].temperatureMin) + "&#176;";
+             var maxTemp = Math.round(result[index].temperatureMax) + "&#176;";
+             var icon_key = result[index].icon;
+             var dewPoint = Math.round(result[index].dewPoint) + "&#176;";
+             var humidity = (result[index].humidity * 100) + "%";
+             var visibility = result[index].visibility + "mi";
+             var windSpeed = Math.round(result[index].windSpeed) + "mph";
+             var pressure = Math.round(result[index].pressure) + "mb";
+             var precipType = result[index].precipType;
+             var precipProb = Math.round(result[index].precipProbability * 100) + '%';
+
+             output += '<span id="weekday">' + day + '</sapn>';
+             output += '<span id="date">' + md + '</span>';
+             output +='</div><div class="sun_info"><ul><li><span class="icon" data-icon="J"></span>';
+             output += '<span class="sunrise">' + srhour + '</span>';
+             output += '</li><li><span class="icon" data-icon="K"></span>';
+             output += '<span class="sunset">' + sshour + '</span>';
+             output += '</li></ul></div></div><div class="more_details"><h1>Details</h1><div class="group"><div class="pattern">';
+             output += '<h2 class="icon" data-icon="' + icons[icon_key] + '"></h2>';
+             output += '</div><div class="specific"><ul>';
+             output += '<li>Dew Point<span>' + dewPoint + '</span></li>';
+             output += '<li>Humidity <span>' + humidity + '</span></li>';
+             if (result[index].hasOwnProperty("visibility")) {
+                output += '<li>Visibility <span>' + visibility + '</span></li>';
+             } else {
+                output += '<li>Pressure<span>' + pressure + '</span></li>';
+             }
+             
+             output += '<li>Wind Speed <span>' + windSpeed + '</span></li>';
+             output += '</ul></div></div><div class="summary"><h2>Next 24 hours</h2>';
+             
+             if (index < result.length-1) {
+                index += 1;
+                output += '<p>' + result[index].summary + ' ';
+             } else {
+                output += '<p>';
+             }
+
+             if (result[index].hasOwnProperty("precipType") && precipType == 'rain') {
+                output += 'Chance of rain ' + precipProb + '. The highest temperatue ' + maxTemp + ' at around ' + maxTempHour + ', and the lowest temperature '+ minTemp +' at around ' + minTempHour + '.</p>';
+             } else if (result[index].hasOwnProperty("precipType") && precipType == 'snow') {
+                output += 'Chance of snow ' + precipProb + '. The highest temperatue ' + maxTemp + ' at around ' + maxTempHour + ', and the lowest temperature '+ minTemp +' at ' + minTempHour + '.</p>';
+             } else {
+                output += 'The highest temperatue ' + maxTemp + ' at around ' + maxTempHour + ', and the lowest temperature '+ minTemp +' at around ' + minTempHour + '.</p>';
+             }
+             output += '</div></div></div>';
+
+             element.append(output);
+          });
       }
 
        function loadCity(city){
@@ -206,7 +312,7 @@ var icons = { "clear-day" : "B",
        $(document).ready(function(){
             loadCity("Seattle");
 
-            $("a.city").bind("click", function(){
+            $("a.city").bind("tap", function(){
                    var city = $(this);
                    var marker = $('#left-panel a').find('i');
                    loadCity(city.text());
